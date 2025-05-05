3507329697f7a59581b5d9271a0ffbdd4856b141
Ronak1926 <ronaksojitra1926@gmail.com>
2025-05-05 21:30:47 +0530

    change in 06_session.js

--- src/06_session.js
+++ src/06_session.js
@@ -1,4 +1,4 @@
-// to run the server in development mode:--> npm run start:dev
+// to run the server in development mode:--> cd src --> nodemon 06_session.js
 
 import express from 'express';
 import routes from './routes/index.js';
@@ -22,7 +22,9 @@ app.use(session({
 app.use(routes);
 
 app.get('/', (request, response) => {
+    console.log('the session object is at / endpoint is :')
     console.log(request.session);
+    console.log('the session id is :')
     console.log(request.session.id)
 
     // console.log(request.sessionID) // this is the same as request.session.id
@@ -44,12 +46,17 @@ app.post('/api/auth',(request,response)=>{
         return response.status(401).send({ msg: "BAD CREDENTIALS" });
     }
 
-    request.session.user = findUser;
+    request.session.user = findUser; // add the user to the session object
     return response.status(200).send(findUser);
 });
 
 app.get('/api/auth/status',(request,response) => {
     request.sessionStore.get(request.sessionID,(err, session)=>{
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        console.log("Inside session store get");
         console.log(session);
     })
     return request.session.user? 
@@ -77,7 +84,11 @@ app.get('/api/cart',(request,response) =>{
     if (!request.session.user) {
         return response.status(401);
     }
+
+    console.log('the session object in /api/cart endpoint is :');
+    console.log(request.session)
     return response.send(request.session.cart ?? []);// if cart is undefined then return []
+
 })
 
 app.listen(PORT, () => {
--- src/routes/users.js
+++ src/routes/users.js
@@ -19,8 +19,12 @@ router.get("/api/users",
      .isLength({ min: 3, max: 10 })
      .withMessage("Must be at least 3-10 characters")
      , (request, response) => {
-    console.log(request.session);
-    console.log(request.session.id);
+
+        console.log('the session object at /api/users is:')
+        console.log(request.session);
+        console.log('the session id at /api/users is :')
+        console.log(request.session.id)
+
     request.sessionStore.get(request.session.id, (err, sessionData)=>{
         if (err) {
             console.log(err);
